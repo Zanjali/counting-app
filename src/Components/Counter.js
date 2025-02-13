c271e0a329c11cf687acbe65dad51c008e6de7bf
Zanjali <anjalilucy123@gmail.com>
2025-02-13 16:45:25 +0530

    m

---
+++ src/Components/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import styles from "./Counter.module.css"; 
+
+const Counter = () => {
+  const [count, setCount] = useState(0); 
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div className={styles.counterApp}>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
